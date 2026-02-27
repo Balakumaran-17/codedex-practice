469617a50ca6a400b975732c98153093a1b6830a
BALAKUMARAN S <balakumaran1708@gmail.com>
2026-02-27 09:39:34 +0530

    Add formatted letter output to 05_letter.py

--- 1-hello-world/05_letter.py
+++ 1-hello-world/05_letter.py
@@ -1,1 +1,8 @@
 
+# Write code below 💖
+print('--------------------------------')
+print('|                    18/02/2026|')
+print('|                     USA      |')
+print('| HI HOW ARE YOU?              |')
+print('|                              |')
+print('--------------------------------')
