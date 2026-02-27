c627a49537674d5d5d0d5cc3b9873ec08c05e6fb
BALAKUMARAN S <balakumaran1708@gmail.com>
2026-02-27 09:43:41 +0530

    Update 05_letter_2.py

--- 1-hello-world/05_letter_2.py
+++ 1-hello-world/05_letter_2.py
@@ -1,1 +1,19 @@
+# Snail Mail 💌
+# Codédex
 
+print('+---------------------------------------------------------------------+')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|        Hi! HELLO BRO WHATSUP  bro                                   |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('|                                          - BALA                     |')
+print('|                                                                     |')
+print('|                                                                     |')
+print('+---------------------------------------------------------------------+')
